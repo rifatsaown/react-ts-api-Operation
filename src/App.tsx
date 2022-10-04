90818b85824984c67ebc5f1f63fb067dc9e220e4
rifatsaown <rifatsaown0@gmail.com>
2022-10-05 05:05:11 +0600

    api loaded

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+interface iUser{
+  name: string;
+  email: string;
+  username: string;
+  phone: string;
+  key?: number;
+  id?: number;
+}
 
 function App() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(res => res.json())
+    .then(data => setUsers(data));
+  },[])
+  
   return (
     <div className="App">
-     <h1>Hello Typescript React</h1>
-     <p>Now we will learn how to call Api</p>
+    <h1>Users Loaded : {users.length}</h1>
+    {
+      users.map((user:iUser) => <User key={user.id} name={user.name} email={user.email} username={user.username} phone={user.phone}></User>)
+    }
     </div>
   );
+}
+
+function User({name, email, username, phone}:iUser) {
+  return (
+    <>
+    <h3>Name : {name}</h3>
+    <h4>Email : {email}</h4>
+    <h4>Username : {username}</h4>
+    <h4>Phone : {phone}</h4>
+    </>
+  )
 }
 
 export default App;
