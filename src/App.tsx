32b040d78154817a3b7e214b4aabb10d3c9c5b41
rifatsaown <rifatsaown0@gmail.com>
2022-10-04 22:01:30 +0600

    Learing API Call

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     
+     <h1>Hello Typescript React</h1>
+     <p>Now we will learn how to call Api</p>
     </div>
   );
 }
