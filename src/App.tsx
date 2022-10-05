39d8da36f06baef6d30eaf89801470eb0abead0d
rifatsaown <rifatsaown0@gmail.com>
2022-10-05 06:04:30 +0600

    recap and practice

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,39 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
-interface iUser {
-  name: string;
-  email: string;
-  username: string;
-  phone: string;
-  key?: number;
-  id?: number;
+
+interface iUser{
+  name:string;
+  email:string;
+  phone:string;
+  key?:number;
+  id?:number;
 }
 
 function App() {
-  const [users, setUsers] = useState([]);
+  const [user, setUser] = useState([]);
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((res) => res.json())
-      .then((data) => setUsers(data));
+      .then((data) => setUser(data));
   }, []);
 
-  return (
-    <div className="App">
-      <h1>Users Loaded : {users.length}</h1>
-      {users.map((user: iUser) => (
-        <User
-          key={user.id}
-          name={user.name}
-          email={user.email}
-          username={user.username}
-          phone={user.phone}
-        ></User>
-      ))}
-    </div>
-  );
+  return(  
+  <div className="App">
+    <h2>User Load : {user.length}</h2>
+    {
+      user.map((user:iUser) => <User name={user.name} email={user.email} phone={user.phone} key={user.id}></User>)
+    }
+  </div>
+  )
 }
-
-function User({ name, email, username, phone }: iUser) {
-  return (
-    <>
-      <div className="userDetails">
-        <h3>Name : {name}</h3>
-        <h4>Email : {email}</h4>
-        <h4>Username : {username}</h4>
-        <h4>Phone : {phone}</h4>
-      </div>
-    </>
-  );
+const User =({name, email,phone}:iUser) => {
+  return(
+    <div className="userDetails">
+      <h3>{name}</h3>
+      <p>{email}</p>
+      <p>{phone}</p>
+    </div>
+  )
 }
 
 export default App;
