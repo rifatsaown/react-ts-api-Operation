bf3869bd1d820915e8d1a47441b564dfa1b40ab5
rifatsaown <rifatsaown0@gmail.com>
2022-10-08 22:39:32 +0600

    alignmrnt

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 
-interface iUser{
-  name:string;
-  email:string;
-  phone:string;
-  key?:number;
-  id?:number;
+interface iUser {
+  name: string;
+  email: string;
+  phone: string;
+  key?: number;
+  id?: number;
 }
 
 function App() {
@@ -17,23 +17,28 @@ function App() {
       .then((data) => setUser(data));
   }, []);
 
-  return(  
-  <div className="App">
-    <h2>User Load : {user.length}</h2>
-    {
-      user.map((user:iUser)=> <User name={user.name} email={user.email} phone={user.phone} key={user.id}></User>)
-    }
-  </div>
-  )
+  return (
+    <div className="App">
+      <h2>User Load : {user.length}</h2>
+      {user.map((user: iUser) => (
+        <User
+          name={user.name}
+          email={user.email}
+          phone={user.phone}
+          key={user.id}
+        ></User>
+      ))}
+    </div>
+  );
 }
-const User =({name, email,phone}:iUser) => {
-  return(
+const User = ({ name, email, phone }: iUser) => {
+  return (
     <div className="userDetails">
       <h3>{name}</h3>
       <p>{email}</p>
       <p>{phone}</p>
     </div>
-  )
-}
+  );
+};
 
 export default App;
